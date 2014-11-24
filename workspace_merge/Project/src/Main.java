2397e77120392ba6b38b92d3bc5199388d104e6d
rubenmarkvoort <rubenmarkvoort@hotmail.com>
2014-11-24 12:15:41 +0100

    branchcommit

--- workspace_merge/Project/src/Main.java
+++ workspace_merge/Project/src/Main.java
@@ -4,4 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Aangepaste tekst, dit wordt ge-commit!!");
 	}
+	
+	public void koekje(){
+		System.out.println("hoi");
+	}
 }
